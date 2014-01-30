44f39229dc92415b721c855b5b3703d272308b2f
Your Name <you@example.com>
2014-01-30 10:36:23 -0500

    first commit

---
+++ test/lein_shell_cat/core_test.clj
@@ -0,0 +1,7 @@
+(ns lein-shell-cat.core-test
+  (:require [clojure.test :refer :all]
+            [lein-shell-cat.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
