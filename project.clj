97f2cdb198e2cf5755434d89ab0dce6ebf281119
rubicks <rubicks@users.noreply.github.com>
2014-01-31 16:04:17 -0500

    now using lein-shell v0.4.0

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :main lein-shell-cat.core
-  :plugins [[lein-shell "0.4.0-SNAPSHOT"]]
+  :plugins [[lein-shell "0.4.0"]]
   :prep-tasks [["shell" "echo" "hello lein-shell"] "javac" "compile"]
   :shell {:commands {"echo" {:pipe-stdin? false}}}
   )
